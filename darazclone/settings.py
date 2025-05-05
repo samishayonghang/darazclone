aaa6c28a2a7a10cc024fd7e3fe0adabc8f756f58
samisha yonghang <158455269+samishayonghang@users.noreply.github.com>
2025-05-05 14:24:43 +0545

    Update settings.py

--- darazclone/settings.py
+++ darazclone/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,13 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR /"daraz"/"static",  # Assumes a 'static' folder exists in your project directory
 ]
 
 # Directory where collected static files are stored (for deployment)
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
